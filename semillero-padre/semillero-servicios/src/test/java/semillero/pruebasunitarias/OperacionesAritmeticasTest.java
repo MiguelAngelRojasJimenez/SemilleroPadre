9877bc7fa680e326dfaeed0f692f1799e4796db7
ingmiguel1108 <ing.miguel.rojas@hotmail.com>
2021-10-19 19:07:53 -0500

    Archivo subido

---
+++ semillero-padre/semillero-servicios/src/test/java/semillero/pruebasunitarias/OperacionesAritmeticasTest.java
@@ -0,0 +1,19 @@
+package semillero.pruebasunitarias;
+
+/**
+ * 
+ * @author Miguel Angel
+ * Descripcion Clase que determina las pruebas unitarias gestinar operaciones aritmeticas
+ *
+ */
+
+
+
+public class OperacionesAritmeticasTest {
+	
+	private String nombre;
+	
+	
+
+	
+}
